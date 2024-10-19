ae9f7bea33bc753f01793b3f4ac0b79a564c54e1
Renon <octobfourtrenn@gmail.com>
2024-10-19 21:47:37 +0900

    Renon ログイン機能実装完了

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'login.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized(); // Firebase初期化前の準備
-  await Firebase.initializeApp(); // Firebaseの初期化
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform, // プラットフォームに応じたオプションを設定
+  );
   runApp(MyApp());
 }
 
